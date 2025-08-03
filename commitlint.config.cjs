903106e3208366866e35c48582a0ab826e9bee4b
retardgerman <78982850+retardgerman@users.noreply.github.com>
2025-08-03 16:33:58 +0200

    chore: trigger release

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Accept chore(release): ... as valid
+    'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'release']],
+    'subject-empty': [2, 'never'],
+  },
 };
