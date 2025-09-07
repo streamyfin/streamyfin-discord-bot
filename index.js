861f4c70d6ada2264922f53d93b8bf3ae5c6396b
retardgerman <78982850+retardgerman@users.noreply.github.com>
2025-09-07 18:29:58 +0200

    chore(index): move imports to top for consistenc
    thx  to @Simon-Eklundh for pointing that out.:D

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import Streamyfin from './client.js';
 import { GatewayIntentBits, REST, Routes, MessageFlags, EmbedBuilder } from 'discord.js';
 import { eldr } from 'eldr';
@@ -37,9 +38,6 @@ const channelsToIgnore = importChannelsToIgnore();
 const client = new Streamyfin({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 });
-
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
